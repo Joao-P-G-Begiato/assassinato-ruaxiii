6391c270ec19f39f5b6657f8ea514b432c2d1ce6
Joao P G Begiato <jpbegiato@hotmail.com>
2022-04-22 00:20:41 -0300

    adiciona indentação ao código

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var nomeDetetive = prompt(`Olá detetive, qual seu nome?`)
 
 
-document.write(`  
-<div class="corpo">
-<h1>Assassinato na Rua XIII.</h1>
-<p>A rua XIII sempre foi conhecida por ser calma e tranquila, uma vizinhança pacata onde até mesmo as 
-crianças brincavam na rua sem causar qualquer preocupação em seus pais, porém essa paz foi quebrada pelos 
-sons de sirenes de carros de polícia que chegavam na casa de Dona Clotilde uma senhora aposentada que morava 
-na rua desde que era criança. E você caro Detetive <em><strong id="detetive"> ${nomeDetetive} </em> </strong>
- foi selecionado para investigar o assassinato que ocorreu
-ali.
-</p>
+document.write(
+`<div class="corpo">
+    <h1>Assassinato na Rua XIII.</h1>
+        <p>
+        A rua XIII sempre foi conhecida por ser calma e tranquila, uma vizinhança pacata onde até mesmo as 
+        crianças brincavam na rua sem causar qualquer preocupação em seus pais, porém essa paz foi quebrada pelos 
+        sons de sirenes de carros de polícia que chegavam na casa de Dona Clotilde uma senhora aposentada que morava 
+        na rua desde que era criança. E você caro Detetive <em><strong id="detetive"> ${nomeDetetive} </em> </strong>
+        foi selecionado para investigar o assassinato que ocorreu ali.
+        </p>
 
-<a href="./entroncamento.html">
-    <button> Iniciar o Jogo </button>
-</a>
-</div>`)
+        <a href="./entroncamento.html">
+            <button> Iniciar o Jogo </button>
+        </a>
+</div>`
+)
--- resultado.js
+++ resultado.js
@@ -1,36 +1,56 @@
 var resultado = prompt("Escolhar o número da opção correta, relembrando: 1- Margareth ; 2- Thiago; 3- Ramon; 4- Margareth E Ramon; 5- Margareth E Thiago; 6- Thiago E Ramon; 7- Margareth, Thiago e Ramon.");
 
   while(isNaN(resultado)){
-    resultado = prompt("Escolhar o número da opção correta.");
+    resultado = prompt("Escolha o número da opção correta.");
     }
 
     while(resultado > 7 || resultado < 1){
-        resultado = prompt("Escolhar o número da opção correta entre 1 e 7.");
+        resultado = prompt("Escolha o número entre as opções validas entre 1 e 7.");
         }
 
 function ganharOuPerder (){
     if(resultado==7){
-        document.write(`   <div class="vitoria">
-        <h1 class="win"> Vitória </h1><br>
-        <h3> Parabéns Detetive, você conseguiu desvendar esse mistério. </h3>
+        document.write(`  
+        <div class="vitoria">
+            <h1 class="win"> Vitória </h1><br>
+            <h3> Parabéns Detetive, você conseguiu desvendar esse mistério. </h3>
 
-        <p>Ao prender os Assassinos eles confessaram o crime:</p>
+            <p>Ao prender os Assassinos eles confessaram o crime:</p>
 
-        O Plano era dar um susto na Clotilde, a Margareth e o Thiago iriam segura-la enquanto Ramon 
-        ameaçava com o tesourão, dizendo para ela começar a tratar as outras pessoas de uma maneira melhor, 
-        porém Clotilde reagiu e conseguiu soltar uma das mãos, fazendo com que Ramon cortasse a mão e Thiago
-         que sem querer esbarrou a mão ensanguentava na manga de Margareth e Clotilde com uma das mãos livres
-          puxou a máscara de Ramon que acabou sendo exposto e decidiu então que precisaria matá-la.
+                O Plano era dar um susto na Clotilde, a Margareth e o Thiago iriam segura-la enquanto Ramon 
+                ameaçava com o tesourão, dizendo para ela começar a tratar as outras pessoas de uma maneira melhor, 
+                porém Clotilde reagiu e conseguiu soltar uma das mãos, fazendo com que Ramon cortasse a mão e Thiago
+                que sem querer esbarrou a mão ensanguentava na manga de Margareth e Clotilde com uma das mãos livres
+                puxou a máscara de Ramon que acabou sendo exposto e decidiu então que precisaria matá-la.
 
-        <h3><a href="./index.html"><button class="bwin"> Retorne ao início</button></a></h3>
-        </div>`)
+            <a href="./index.html">
+                <button class="bwin">
+                    Retorne ao início
+                </button>
+            </a>
+        </div>
+        `)
     } else{
-        document.write(`<div class="gameover">
-        <h1> GAME OVER </h1><br>
-        <h3> Você deixou o assasino fugir</h3>
-        <h3> tente novamnte </h3>
-        <a href="./index.html"><button> Retorne ao início</button></a>
-        </div>`)
+        document.write(`
+        <div class="gameover">
+            <h1> GAME OVER </h1><br>
+            <h3> 
+            Você deixou o assasino fugir<br><br><br>
+             Tente novamnte. <br><br>
+                <em>
+                 Dica:<br>
+                </em>
+                Tente coletar mais informações interrogando outros suspeitos, isso fará voê descobrir novas 
+                informações<br><br>
+            </h3>
+
+            <a href="./index.html">
+                <button>
+                     Retorne ao início
+                 </button>
+            </a>
+        </div>
+        `)
     }
 }
 
